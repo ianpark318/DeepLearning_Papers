ba853cf740c84fccbb1e7c6ac739789f57def023
ianpark318 <ianpark318@gmail.com>
2023-01-16 22:25:24 +0900

    making model

---
+++ SSD/utils.py
@@ -0,0 +1,324 @@
+import json
+import os
+import torch
+import random
+import xml.etree.ElementTree as ET
+import torchvision.transforms.functional as FT
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Label map
+voc_labels = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+              'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+label_map = {k: v for v, k in enumerate(voc_labels, 1)}
+label_map['background'] = 0
+rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
+
+# Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+                   '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000',
+                   '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']
+label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+
+
+def parse_annotation(annotation_path):
+    tree = ET.parse(annotation_path)
+    root = tree.getroot()
+
+    boxes = []
+    labels = []
+    difficulties = []
+    for object in root.iter('object'):
+        difficult = int(object.find('difficult').text == '1')
+
+        label = object.find('name').text.lower().strip()
+        if label not in label_map:
+            continue
+
+        bbox = object.find('bndbox')
+        xmin = int(bbox.find('xmin').text) - 1
+        ymin = int(bbox.find('ymin').text) - 1
+        xmax = int(bbox.find('xmax').text) - 1
+        ymax = int(bbox.find('ymax').text) - 1
+
+        boxes.append([xmin, ymin, xmax, ymax])
+        labels.append(label_map[label])
+        difficulties.append(difficult)
+        print(len(boxes))
+
+    return {'boxes': boxes, 'labels': labels, 'difficulties': difficulties}
+
+
+def create_data_lists(voc07_path, output_folder):
+    """
+    Create lists of images, the bounding boxes and labels of the objects in these images, and save these to file.
+
+    :param voc07_path: path to the 'VOC2007' folder
+    :param voc12_path: path to the 'VOC2012' folder
+    :param output_folder: folder where the JSONs must be saved
+    """
+    voc07_path = os.path.abspath(voc07_path)
+
+    train_images = []
+    train_objects = []
+    n_objects = 0
+
+    # Training data
+
+    # Find IDs of images in training data
+    with open(os.path.join(voc07_path, 'ImageSets/Main/trainval.txt')) as f:
+        ids = f.read().splitlines()
+
+    for id in ids:
+        # Parse annotation's XML file
+        objects = parse_annotation(os.path.join(voc07_path, 'Annotations', id + '.xml'))
+        if len(objects['boxes']) == 0:
+            continue
+        n_objects += len(objects)
+        train_objects.append(objects)
+        train_images.append(os.path.join(voc07_path, 'JPEGImages', id + '.jpg'))
+
+    assert len(train_objects) == len(train_images)
+
+    # Save to file
+    with open(os.path.join(output_folder, 'TRAIN_images.json'), 'w') as j:
+        json.dump(train_images, j)
+    with open(os.path.join(output_folder, 'TRAIN_objects.json'), 'w') as j:
+        json.dump(train_objects, j)
+    with open(os.path.join(output_folder, 'label_map.json'), 'w') as j:
+        json.dump(label_map, j)  # save label map too
+    print('\nThere are %d training images containing a total of %d objects. Files have been saved to %s.' % (
+        len(train_images), n_objects, os.path.abspath(output_folder)))
+
+    # Test data
+    test_images = []
+    test_objects = []
+    n_objects = 0
+
+    # Find IDs of images in the test data
+    with open(os.path.join(voc07_path, 'ImageSets/Main/test.txt')) as f:
+        ids = f.read().splitlines()
+
+    for id in ids:
+        # Parse annotation's XML file
+        objects = parse_annotation(os.path.join(voc07_path, 'Annotations', id + '.xml'))
+        if len(objects) == 0:
+            continue
+        test_objects.append(objects)
+        n_objects += len(objects)
+        test_images.append(os.path.join(voc07_path, 'JPEGImages', id + '.jpg'))
+
+    assert len(test_objects) == len(test_images)
+
+    # Save to file
+    with open(os.path.join(output_folder, 'TEST_images.json'), 'w') as j:
+        json.dump(test_images, j)
+    with open(os.path.join(output_folder, 'TEST_objects.json'), 'w') as j:
+        json.dump(test_objects, j)
+    print('\nThere are %d test images containing a total of %d objects. Files have been saved to %s.' % (
+        len(test_images), n_objects, os.path.abspath(output_folder)))
+
+
+def decimate(tensor, m):
+    """
+    Decimate a tensor by a factor 'm', i.e. downsample by keeping every 'm'th value.
+    This is used when we convert FC layers to equivalent Convolutional layers, BUT of a smaller size.
+
+    :param tensor: tensor to be decimated
+    :param m: list of decimation factors for each dimension of the tensor. None if not to be decimated along a dimension
+    :return: decimated tensor
+    """
+    assert tensor.dim() == len(m)
+    for d in range(tensor.dim()):
+        if m[d] is not None:
+            tensor = tensor.index_select(dim=d, index=torch.arange(start=0, end=tensor.size(d), step=m[d]).long())
+
+    return tensor
+
+def expand(image, boxes, filler):
+    """
+    Perform a zomming out  operation by placing the image in a larger canvas of filler material.
+
+    Helps to learn to detect smaller objects.
+
+    :param image: image, a tensor of dimensions (3, original_h, original_w)
+    :param boxes: bounding boxes in boundary coordinates, a tensor of dimensions (n_objects, 4)
+    :param filler: RGB values of the filler material, a list like [R, G, B]
+    :return: expanded image, updated bounding box coordinates
+    """
+    # Calculate dimensions of proposed expanded (zoomed-out) image
+    original_h = image.shape[1]
+    original_w = image.shape[2]
+    max_scale = 4
+    scale = random.uniform(1, max_scale)
+    new_h = int(scale * original_h)
+    new_w = int(scale * original_w)
+
+    # Create such an image with the filler
+    filler = torch.FloatTensor(filler)
+    new_image = torch.ones((3, new_h, new_w), dtype=torch.float) * filler.unsqueeze(1).unsqueeze(1)
+
+    # Place the original image at random coordinates in this new image (origin at top-left of image)
+    left = random.randint(0, new_w - original_w)
+    right = left + original_w
+    top = random.randint(0, new_h - original_h)
+    bottom = top + original_h
+    new_image[:, top:bottom, left:right] = image
+
+    # Adjusting bounding boxes' coordinates accordingly
+    new_boxes = boxes + torch.FloatTensor([left, top, left, top]).unsqueeze(0)
+
+    return new_image, new_boxes
+
+
+def random_crop(image, boxes, labels, difficulties):
+    """
+    Perfroms a random crop in the manner stated in the paper.
+    Helps to learn to detect larger and partial objects.
+    Note that some objects may be cut out entirely.
+
+    :param image: image, a tensor of dimensions (3, original_h, original_w)
+    :param boxes: bounding boxes in boundary coordinates, a tensor of dimensions (n_objects, 4)
+    :param labels: labels of objects, a tensor of dimensions (n_objects)
+    :param difficulties: difficulties of detection of these objects, a tensor of dimensions (n_objects)
+    :return: cropped image, updated bounding box coordinates, updated labels, updated difficulties
+    """
+    original_h = image.shape[1]
+    original_w = image.shape[2]
+    # Keep choosing a minimum overlap until a successful crop is made
+
+
+def flip(image, boxes):
+    """
+    Flip image horizontally.
+
+    :param iamge: image, a PIL Image
+    :param boxes: bounding boxes in boundary coordinates, a tensor of dimensions (n_objects, 4)
+    :return: flipped image, updated bounding box coordinates
+    """
+    # Flip image
+    new_image = FT.hflip(image)
+
+    # Flip boxes
+    # Why? 여기 이해 잘 안됨
+    new_boxes = boxes
+    new_boxes[:, 0] = image.width - boxes[:, 0] - 1
+    new_boxes[:, 2] = image.width - boxes[:, 2] - 1
+    new_boxes = new_boxes[:, [2, 1, 0, 3]]
+
+    return new_image, new_boxes
+
+
+def resize(image, boxes, dims=(300, 300), return_percent_coords=True):
+    """
+    Resize image. For the SSD300, resize to (300, 300).
+
+    Since percent/fractional coordinates are calculated for the bounding boxes (w.r.t image dimensions)
+    in this process, you may choose to retain them.
+    :param image: image, a PIL Image
+    :param boxes: bounding boxes in boundary coordinates, a tensor of dimensions (n_objects, 4)
+    :return: resized image, updated b-box coordinates (or fractional coordinates, in which case they remain the same)
+    """
+    # Resize image
+    new_image = FT.resize(image, dims)
+
+    # Resize bounding boxes
+    # 이거도 이해 안됨..
+    old_dims = torch.FloatTensor([image.width, image.height, image.width, image.height]).unsqueeze(0)
+    new_boxes = boxes / old_dims   # percent coordinates
+
+    if not return_percent_coords:
+        new_dims = torch.FloatTensor([dims[1], dims[0], dims[1], dims[0]]).unsqueeze(0)
+        new_boxes = new_boxes * new_dims
+
+    return new_image, new_boxes
+
+
+def photometric_distort(image):
+    """
+    Distort brightness, contrast, saturation, and hue, each with a 50% chance, in random order.
+
+    :param image: image, a PIL Image
+    :return: distorted image
+    """
+    new_image = image
+
+    distortions = [FT.adjust_brightness,
+                   FT.adjust_contrast,
+                   FT.adjust_saturation,
+                   FT.adjust_hue]
+
+    random.shuffle(distortions)
+
+    for d in distortions:
+        if random.random() < 0.5:
+            if d.__name__ == 'adjust_hue':
+                # Caffe repo uses a 'hue_delta' of 18 - we divide by 255 because PyTorch needs a normalized value
+                adjust_factor = random.uniform(-18 / 255., 18 / 255.)
+            else:
+                # Caffe repo uses 'lower' and 'upper' values of 0.5 and 1.5 for brightness, contrast, and saturation
+                adjust_factor = random.uniform(0.5, 1.5)
+
+            # Apply this distortion
+            new_image = d(new_image, adjust_factor)
+
+    return new_image
+
+
+def transform(image, boxes, labels, difficulties, split):
+    """
+    Apply the transformations above.
+
+    :param image: image, a PIL Image
+    :param boxes: bounding boxes in boundary coordinates, a tensor of demensions (n_objects, 4)
+    :param labels: labels of objects, a tensor of dimensions (n_objects)
+    :param difficulties: difficulties of detection of these objects, a tensor of dimensions (n_objects)
+    :param split: one of 'TRAIN' or 'TEST', since different sets of transformations are applied
+    :return: transformed image, transformed bounding box coordinates, transformed labels, transformed difficulties
+    """
+    assert split in {'TRAIN', 'TEST'}
+
+    # Mean and standard deviation of ImageNet data that our base VGG from torchvision was trained on
+    # see: https://pytorch.org/docs/stable/torchvision/models.html
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    new_image = image
+    new_boxes = boxes
+    new_labels = labels
+    new_difficulties = difficulties
+
+    # Skip the following operations for evaluation/testing
+    if split == 'TRAIN':
+        # A series of photometric distortions in random order, each with 50% chance of occurence, as in Caffe repo
+        new_image = photometric_distort(new_image)
+
+        # Conver PIL Image to Torch tensor
+        new_image = FT.to_tensor(new_image)
+
+        # Expand image (zoom out) with a 50% chance - helpful for training detection of small objects
+        # Fill surrounding space with the mean of ImageNet data that our base VGG  was trained on
+        if random.random() < 0.5:
+            new_image, new_boxes = expand(new_image, boxes, filler=mean)
+
+        # Randomly crop image (zoom in)
+        #new_image, new_boxes, new_labels, new_difficulties = random_crop(new_image, new_boxes, new_labels,
+        #                                                                 new_difficulties)
+
+        # Convert Torch tensor to PIL Image
+        new_image = FT.to_pil_image(new_image)
+
+        # Flip image with a 50% chance
+        if random.random() < 0.5:
+            new_image, new_boxes = flip(new_image, new_boxes)
+
+    # Resize image to (300, 300) - this also converts absolute boundary coordinates to their fractional form
+    new_image, new_boxes = resize(new_image, new_boxes, dims=(300, 300))
+
+    # Convert PIL image to Torch tensor
+    new_image = FT.to_tensor(new_image)
+
+    # Normalize by mean and standard deviation of ImageNet data that our base VGG was trained on
+    new_image = FT.normalize(new_image, mean=mean, std=std)
+
+    return new_image, new_boxes, new_labels, new_difficulties
